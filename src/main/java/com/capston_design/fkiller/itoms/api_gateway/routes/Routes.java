02e8675bf959af315fefa8bf978a8cf81c871835
박성준 <sungjunp1208@gmail.com>
2025-05-28 22:01:25 +0900

    feat: aggregate rest api documentation

--- src/main/java/com/capston_design/fkiller/itoms/api_gateway/routes/Routes.java
+++ src/main/java/com/capston_design/fkiller/itoms/api_gateway/routes/Routes.java
@@ -4,18 +4,27 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.function.HandlerFunction;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 
 @Configuration
 public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> serviceDeskRoute() {
-        return GatewayRouterFunctions.route("product_service")
+        return GatewayRouterFunctions.route("service_desk")
                 .route(RequestPredicates.path("/api/incident"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> serviceDeskSwaggerRoute() {
+        return GatewayRouterFunctions.route("service_desk_swagger")
+                .route(RequestPredicates.path("/aggregate/service-desk/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
@@ -25,4 +34,14 @@ public class Routes {
                 .route(RequestPredicates.path("/api/user"), HandlerFunctions.http("http://localhost:8081"))
                 .build();
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceSwaggerRoute() {
+        return GatewayRouterFunctions.route("user_service_swagger")
+                .route(RequestPredicates.path("/aggregate/user-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+
 }
